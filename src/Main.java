40119f33a65ed5d48a197b859dd00e24edfdf5a5
tokyorider <noobfromrus920@gmail.com>
2019-10-15 23:05:07 +0700

    trying to fix encodings

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         }
 
         file = new File(args[1]);
-        Client client = new Client("Иванов", "Иван", "Иванович");
+        Client client = new Client("Иванов", "Иван", "Иван");
         ConsumerBasket basket;
         try {
             basket = new ConsumerBasket(file, client, directory, 1);
